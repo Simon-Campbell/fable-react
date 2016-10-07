65f16a3c202b782de6e6cd598c69f43b14d03284
Simon Campbell <simon.s.campbell@gmail.com>
2016-10-07 21:36:31 +1300

    Reconfigure and reduce output bundle.js filesize

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-    devtool: "source-map",
+    devtool: "cheap-source-map",
     entry: "./temp/FableReact.js",
     output: {
         path: path.join(__dirname, "public"),
@@ -15,6 +15,19 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "source-map-loader"
             }
-        ]
-    }
+        ],
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                // This has effect on the react lib size
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 };
